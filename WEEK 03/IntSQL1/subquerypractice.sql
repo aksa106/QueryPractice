ad119fbff66cce11f62c52eeeb73d5b37498d2ae
Aksa <aksasaifi33@gmail.com>
2026-07-07 18:47:28 +0530

    added

---
+++ WEEK 03/IntSQL1/subquerypractice.sql
@@ -0,0 +1,33 @@
+
+
+-- Find the names of instructors whose salary is greater than the average salary of all instructors
+
+select name from instructor 
+where salary>(
+    select avg(salary) 
+    from instructor
+)
+
+
+-- Find the names of instructors whose salary is equal to the highest salary in the instructor table.
+
+
+select name from instructor 
+where salary = (
+    select max(salary)
+    from instructor
+)
+
+-- Find the names and salaries of instructors whose salary is less than the minimum salary in the 'Comp. Sci.' department.
+
+
+select name, salary 
+from instructor 
+where salary <(
+    select min(salary) 
+    from instructor 
+    where dept_name = 'Comp. Sci.'
+)
+
+
+
